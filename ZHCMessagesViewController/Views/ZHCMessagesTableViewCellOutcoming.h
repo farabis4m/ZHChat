01b76f66731d43c4640f69550669cf49afa3ddc8
Jafar Khan <jafar@farabi.ae>
2018-01-15 14:21:02 +0400

    added constraint in cell out coming base class ENBD-4033 (reverted from commit 3a10fe8f2c5e889dbd0ed4852890ed6a6116e1bc)

--- ZHCMessagesViewController/Views/ZHCMessagesTableViewCellOutcoming.h
+++ ZHCMessagesViewController/Views/ZHCMessagesTableViewCellOutcoming.h
@@ -11,8 +11,6 @@
 
 @interface ZHCMessagesTableViewCellOutcoming : ZHCMessagesTableViewCell
 @property (weak, nonatomic) IBOutlet UIButton *editButton;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintTextViewTrailing;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintMessageBubbleLeading;
 
 
 
